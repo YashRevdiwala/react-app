eb341b77261bf883be67efea45951c96ab52696c
yash-0409 <yash.yr.2493@gmail.com>
2023-05-16 22:46:49 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,77 @@
-const wrt = "Akshat Gupta"
-const tle = "The Hidden Hindu"
-const img =
-  "https://images-eu.ssl-images-amazon.com/images/I/81ZI8RyyynL._AC_UL600_SR600,400_.jpg"
+const firstBook = {
+  writer: "Akshat Gupta",
+  title: "The Hidden Hindu",
+  img: "/assets/the-hidden-hindu.jpg",
+}
+const secondBook = {
+  writer: "A.C. Bhaktivendanta Swami Prabhupada",
+  title: "Bhagwat Gita",
+  img: "https://images-eu.ssl-images-amazon.com/images/I/91msEbTletL._AC_UL600_SR600,400_.jpg",
+}
+const thirdBook = {
+  writer: "Faf Du Plessis",
+  title: "Faf Through Fire",
+  img: "",
+}
+const fourthBook = {
+  writer: "Robert T. Kiyosaki",
+  title: "Rich Dad Poor Dad",
+  img: "",
+}
+const fifthBook = {
+  writer: "Francesc Miralles",
+  title: "Ikigai",
+  img: "",
+}
+const sixthBook = {
+  writer: "Robert Greene",
+  title: "The 48 Laws of Power",
+  img: "",
+}
+const seventhBook = {
+  writer: "James Clear",
+  title: "Atomic Habits",
+  img: "",
+}
 
 const BookList = () => {
   return (
     <div className="booklist">
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
-      <Book image={img} writer={wrt} title={tle} />
+      <Book
+        image={firstBook.img}
+        writer={firstBook.writer}
+        title={firstBook.title}
+      />
+      <Book
+        image={secondBook.img}
+        writer={secondBook.writer}
+        title={secondBook.title}
+      />
+      <Book
+        image={thirdBook.img}
+        writer={thirdBook.writer}
+        title={thirdBook.title}
+      />
+      <Book
+        image={fourthBook.img}
+        writer={fourthBook.writer}
+        title={fourthBook.title}
+      />
+      <Book
+        image={fifthBook.img}
+        writer={fifthBook.writer}
+        title={fifthBook.title}
+      />
+      <Book
+        image={sixthBook.img}
+        writer={sixthBook.writer}
+        title={sixthBook.title}
+      />
+      <Book
+        image={seventhBook.img}
+        writer={seventhBook.writer}
+        title={seventhBook.title}
+      />
     </div>
   )
 }
