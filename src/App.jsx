51fcfdd15533c7d3b6d1ca3da01c273e0e32e171
yash-0409 <yash.yr.2493@gmail.com>
2023-09-07 20:59:56 +0530

    dynamic props

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,61 @@
-const firstBook = {
-  writer: "Akshat Gupta",
-  title: "The Hidden Hindu",
-  img: "/assets/the-hidden-hindu.jpg",
-}
-const secondBook = {
-  writer: "A.C. Bhaktivendanta Swami Prabhupada",
-  title: "Bhagwat Gita",
-  img: "/assets/bhagwat-gita.jpg",
-}
-const thirdBook = {
-  writer: "Faf Du Plessis",
-  title: "Faf Through Fire",
-  img: "/assets/faf.jpg",
-}
-const fourthBook = {
-  writer: "Robert T. Kiyosaki",
-  title: "Rich Dad Poor Dad",
-  img: "/assets/rich-dad-poor-dad.jpg",
-}
-const fifthBook = {
-  writer: "Francesc Miralles",
-  title: "Ikigai",
-  img: "/assets/ikigai.jpg",
-}
-const sixthBook = {
-  writer: "Robert Greene",
-  title: "The 48 Laws of Power",
-  img: "/assets/laws-of-power.jpg",
-}
-const seventhBook = {
-  writer: "James Clear",
-  title: "Atomic Habits",
-  img: "/assets/atomic-habits.jpg",
-}
+const books = [
+  {
+    writer: "Akshat Gupta",
+    title: "The Hidden Hindu",
+    img: "/assets/the-hidden-hindu.jpg",
+  },
+  {
+    writer: "A.C. Bhaktivendanta Swami Prabhupada",
+    title: "Bhagwat Gita",
+    img: "/assets/bhagwat-gita.jpg",
+  },
+  {
+    writer: "Faf Du Plessis",
+    title: "Faf Through Fire",
+    img: "/assets/faf.jpg",
+  },
+  {
+    writer: "Robert T. Kiyosaki",
+    title: "Rich Dad Poor Dad",
+    img: "/assets/rich-dad-poor-dad.jpg",
+  },
+  {
+    writer: "Francesc Miralles",
+    title: "Ikigai",
+    img: "/assets/ikigai.jpg",
+  },
+  {
+    writer: "Robert Greene",
+    title: "The 48 Laws of Power",
+    img: "/assets/laws-of-power.jpg",
+  },
+  {
+    writer: "James Clear",
+    title: "Atomic Habits",
+    img: "/assets/atomic-habits.jpg",
+  },
+]
 
 const BookList = () => {
   return (
     <div className="booklist">
-      <Book
-        image={firstBook.img}
-        writer={firstBook.writer}
-        title={firstBook.title}
-      />
-      <Book
-        image={secondBook.img}
-        writer={secondBook.writer}
-        title={secondBook.title}
-      />
-      <Book
-        image={thirdBook.img}
-        writer={thirdBook.writer}
-        title={thirdBook.title}
-      />
-      <Book
-        image={fourthBook.img}
-        writer={fourthBook.writer}
-        title={fourthBook.title}
-      />
-      <Book
-        image={fifthBook.img}
-        writer={fifthBook.writer}
-        title={fifthBook.title}
-      />
-      <Book
-        image={sixthBook.img}
-        writer={sixthBook.writer}
-        title={sixthBook.title}
-      />
-      <Book
-        image={seventhBook.img}
-        writer={seventhBook.writer}
-        title={seventhBook.title}
-      />
+      {books.map((data) => {
+        const { img, writer, title } = data
+        // eslint-disable-next-line react/jsx-key
+        return <Book image={img} writer={writer} title={title} />
+      })}
     </div>
   )
 }
 
+// eslint-disable-next-line react/prop-types
 const Book = ({ image, writer, title }) => {
   return (
-    <div className="book">
+    <article className="book">
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <h4>{writer}</h4>
-    </div>
+    </article>
   )
 }
 
